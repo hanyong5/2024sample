40b83e82469f332b1aa6c40838bd6d4f96142f2b
hanyong5 <hanyong5@naver.com>
2024-01-10 12:21:27 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+      praesentium unde recusandae error nobis, facere in ipsam qui. Non
+      doloremque debitis fugiat, libero ducimus doloribus minima. Ipsa
+      voluptatem saepe autem!
+    </div>
+  );
 }
 
 export default App;
